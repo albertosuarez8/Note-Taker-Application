7505ea110b3f64d7f8576c98594fb3ae25a6dd72
Albert <alberto.suarez8@outlook.com>
2022-12-11 17:03:22 -0500

    Added Server JS File and Functionality

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('./db/db.json')
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = 3001;
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+  });
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
